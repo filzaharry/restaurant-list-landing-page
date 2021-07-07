f4dea7056281ab187b1abc4683baad5a0a4cac91
filza harry <filza.harry@missiidea.com>
2021-07-07 11:47:33 +0700

    fixing submission 1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,35 @@
 import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/navigation-drawer.css';
 import '../styles/main.css';
+import '../styles/responsive.css';
 
 console.log('Hello Coders! :)');
+
+fetch('../DATA.json')
+  .then(response => response.json())
+  .then(data => getRestaurants(data));
+
+function getRestaurants(data) {
+  let restaurantHTML = "";
+  data.restaurants.forEach(function (restaurant, index) {
+    // console.log(restaurant);
+    restaurantHTML += `
+      <div tabindex="0" class="cards-section">
+        <div class="card">
+          <button  class="card-rating">${restaurant.rating}</button>
+          <img tabindex="0" class="img-res" alt="${restaurant.name}" src="${restaurant.pictureId}"/>
+          
+          <p  class="card-city">${restaurant.city}</p>
+          <div tabindex="0" class="in-card">
+            <p tabindex="0" class="card-title">${restaurant.name}</p>
+            <p class="card-content-title">Description :</p>
+            <p class="truncate">${restaurant.description}</p>
+            <p class="read-more">Read More ...</p>
+          </div>
+        </div>
+      </div>  
+      `;
+  });
+
+  document.getElementById("resto-list").innerHTML = restaurantHTML;
+}
