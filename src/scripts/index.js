3150b38e2d76e2a1ea9fe065dc095709f813e31e
filza harry <filza.harry@missiidea.com>
2021-07-16 22:59:52 +0700

    update all

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,6 @@ import '../styles/navigation-drawer.css';
 import '../styles/main.css';
 import '../styles/responsive.css';
 import '../styles/skip-to-content.css';
-import '../styles/hamburger.css';
 
 console.log('Hello Coders! :)');
 
